96973536c7544c266f92347bd5e1b828777ca11c
unknown <douglas.luzinda.ot@gmail.com>
2023-05-27 20:59:24 +0300

    join-and-mission

--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -2,50 +2,31 @@ import React from "react";
 import "../styles.css";
 import { useSelector } from "react-redux";
 
+const getReseved = (rocket) => {
+  if (rocket.reserved) {
+    return (
+      <tr key={rocket}>
+        <td className="separator">{rocket.rocket_name}</td>
+      </tr>
+    );
+  }
+  return null;
+};
 function MyProfilePage() {
   const { rockets } = useSelector((state) => state.rockets);
-  const { missions } = useSelector((state) => state.mission);
-
+  // const { missions } = useSelector((state) => state.missions);
   return (
     <div className="main-wrap">
       <div className="list-wrap">
         <h3>My Missions</h3>
-        <table className="bordered rounded">
-
-          {missions.filter((mission) => mission.active === true).map((mission) => (
-            <tr key={mission}>
-              <td className="separator">{mission.mission_name}</td>
-            </tr>
-          ))}
-          {missions.length === 0 && (
-            <tr>
-              <td>
-                <p style={{ marginLeft: "20px", marginBottom: "20px" }}>
-                  No joined missions data to display
-                </p>
-              </td>
-            </tr>
-          )}
-
-        </table>
+        <table className="bordered rounded" />
       </div>
       <div className="list-wrapx">
         <h3>My Rockets</h3>
         <table className="bordered rounded">
-          {rockets.filter((rock) => rock.reserved === true).map((rocket) => (
-            <tr key={rocket}>
-              <td className="separator">{rocket.rocket_name}</td>
-            </tr>
+          {rockets.map((rocket) => (
+            getReseved(rocket)
           ))}
-          {rockets.length === 0 && (
-            <tr>
-              <td>
-                <p style={{ marginLeft: "20px", marginBottom: "20px" }}>
-                  No reserved rocket data to display
-                </p>
-              </td>
-            </tr>
-          )}
         </table>
       </div>
     </div>
